1c93da43f78315e380895ea99d5bacd5a29cdcc0
JustProgramer <vladmerkv@gmail.com>
2025-01-26 18:48:54 +0300

    some for alexan

--- AntlrLexerParser/src/main/java/bytecode/bytecodeHolder.java
+++ AntlrLexerParser/src/main/java/bytecode/bytecodeHolder.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 @Getter
@@ -12,8 +13,9 @@ import java.util.Map;
 @AllArgsConstructor
 public class bytecodeHolder {
     private ArrayList<bytecode> bytecodes;
-    private Map<Integer, String> stringTable;
+    private Map<String,Integer> stringTable;
     public bytecodeHolder() {
         bytecodes = new ArrayList<bytecode>();
+        stringTable = new HashMap<String,Integer>();
     }
 }
