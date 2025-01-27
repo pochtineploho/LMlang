5a2d640d4bff02ca025e15c97ac942607e017535
JustProgramer <vladmerkv@gmail.com>
2025-01-27 07:04:50 +0300

    a

--- AntlrLexerParser/src/main/java/ast/ParameterListNode.java
+++ AntlrLexerParser/src/main/java/ast/ParameterListNode.java
@@ -32,7 +32,9 @@ public class ParameterListNode implements ASTNode {
     @Override
     public void BytecodeGeneration(bytecodeHolder bch){
         for (ASTNode argument : parameters) {
-            argument.BytecodeGeneration(bch);
+            if(argument != null){
+                argument.BytecodeGeneration(bch);
+            }
         }
     }
 }
